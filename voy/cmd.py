d73e02c77467c08160867ccfce85f3265406b4f9
Florin Gogianu <florin.gogianu@gmail.com>
2024-03-10 08:45:31 +0200

    Remove comment.

--- voy/cmd.py
+++ voy/cmd.py
@@ -1,12 +1,11 @@
-""" Here we define wrapper functions to be called when one invokes
-    console commands.
+"""Here we define wrapper functions to be called when one invokes
+console commands.
 """
 
 # pylint: disable=import-outside-toplevel
 
 
 def voy():
-    """liftoff-prepare"""
     from .voy import main as _voy
 
     _voy()
